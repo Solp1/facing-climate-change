15fb220f239b72cb402c21ae861769544b21784c
картофелина <gabrielle.multan.pro@gmail.com>
2023-12-07 19:46:45 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <v-app>
+      <v-main id="app">
+        <NuxtPage />
+      </v-main>
+    </v-app>
+  </NuxtLayout>
+</template>
+<script setup lang="ts">
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  //...
+  $meta: {
+    title: "Facing Climate Change",
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  css: ["~/assets/style.css","vuetify/lib/styles/main.sass"],
+  modules: [
+    '@nuxt/content',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
